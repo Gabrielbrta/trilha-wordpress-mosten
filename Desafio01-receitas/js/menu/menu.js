d0411da7a4e9f27f289d7bf799136c7d7bd51d3e
Gabrielbrta <vdvsa159946@gmail.com>
2025-09-13 22:47:10 -0300

    estilo, menu e responsividade da página home

---
+++ Desafio01-receitas/js/menu/menu.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu');
+const open = document.querySelector('.menu-actions i.open')
+const close = document.querySelector('.menu-actions i.close');
+
+[open, close].forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if(btn.classList.contains('open')) {
+            menu.classList.remove('closed');
+            open.classList.remove('active')
+            close.classList.add('active');
+        } else {
+            close.classList.remove('active');
+            open.classList.add('active')
+            menu.classList.add('closed');
+        }
+    });
+});
