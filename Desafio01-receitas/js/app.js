c21a524d5b3bad109b6d0b39b79991a9b70820c2
Gabrielbrta <vdvsa159946@gmail.com>
2025-09-14 20:12:33 -0300

    estilo base completo

---
+++ Desafio01-receitas/js/app.js
@@ -0,0 +1,3 @@
+import initMenu from "./menu/menu.js";
+
+initMenu();
--- Desafio01-receitas/js/menu/menu.js
+++ Desafio01-receitas/js/menu/menu.js
@@ -1,17 +1,26 @@
-const menu = document.querySelector('.menu');
-const open = document.querySelector('.menu-actions i.open')
-const close = document.querySelector('.menu-actions i.close');
 
-[open, close].forEach((btn) => {
-    btn.addEventListener("click", () => {
-        if(btn.classList.contains('open')) {
-            menu.classList.remove('closed');
-            open.classList.remove('active')
-            close.classList.add('active');
-        } else {
-            close.classList.remove('active');
-            open.classList.add('active')
-            menu.classList.add('closed');
-        }
+function menu() {
+    const menu = document.querySelector('.menu');
+    const open = document.querySelector('.menu-actions i.open')
+    const close = document.querySelector('.menu-actions i.close');
+    
+    [open, close].forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if(btn.classList.contains('open')) {
+                menu.classList.remove('closed');
+                open.classList.remove('active')
+                close.classList.add('active');
+            } else {
+                close.classList.remove('active');
+                open.classList.add('active')
+                menu.classList.add('closed');
+            }
+        });
     });
-});
+}
+
+function initMenu() {
+    menu();
+}
+
+export default initMenu;
